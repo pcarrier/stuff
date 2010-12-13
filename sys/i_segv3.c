2bbfc2a8ba6ad6947d77c8857132a2c0920d06f1
Pierre Carrier <pcarrier@balzac.usersys.redhat.com>
2010-12-13 10:17:19 +0000

    [sys] Weird pthreads bug!?

---
+++ sys/i_segv3.c
@@ -0,0 +1,36 @@
+/**
+ * I segfault (multithreaded)
+ *
+ * Copyright (c) 2010, Red Hat Inc.
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided that
+ * the above copyright notice and this permission notice appear in all copies.
+ *
+ * Maintainer: Pierre Carrier <prc@redhat.com>
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <errno.h>
+
+void *blurp(void *data)
+{
+    sleep(3600);
+    return data;
+}
+
+int main()
+{
+    pthread_t thread0, thread1;
+    if (pthread_create(&thread0, NULL, blurp, NULL) ||
+        pthread_create(&thread1, NULL, blurp, NULL))
+        return errno;
+    if (pthread_kill(thread0, SIGSEGV) || pthread_kill(thread1, SIGSEGV))
+        return -errno;
+#ifdef FIX
+    usleep(0);
+#endif
+    return EXIT_SUCCESS;        /* Whaaat? */
+}
