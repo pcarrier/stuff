5b88247c59ff14e312060d3a301da4233a4f98a1
Pierre Carrier <pcarrier@balzac.usersys.redhat.com>
2010-12-15 12:12:51 +0000

    [crap] weird_gcc.c: more sh*t to explain

--- crap/weird_gcc.c
+++ crap/weird_gcc.c
@@ -7,15 +7,25 @@
 
 int main()
 {
-    void *ptr;
-    ptr = malloc(FOO);
-    assert(ptr != NULL);
-    ptr = malloc(BAR);
-    assert(ptr != NULL);
+    void *ptr1, *ptr2;
+    ptr1 = malloc(FOO*sizeof(char));
+    ptr2 = malloc(BAR*sizeof(char));
+    assert(ptr1 != NULL);
+    assert(ptr2 != NULL);
     return EXIT_SUCCESS;
 }
 
 /* Can someone explain:
+ *
  * crap/weird_gcc.c: In function ‘main’:
  * crap/weird_gcc.c:11: warning: integer overflow in expression
+ *
+ * >>> 2*1024*1024*1024
+ * 2147483648
+ * >>> 1<<31
+ * 2147483648
+ *
+ * ltrace:
+ * malloc(18446744071562067968) = NULL
+ * malloc(18446744071562067968) = NULL
 */
