fd9c5f0a3a7fef8e562e887ebaa423cec07016ed
Pierre Carrier <pcarrier@balzac.usersys.redhat.com>
2010-12-17 16:16:14 +0000

    [fun] superglob.c: of course it has a maintainer

--- fun/superglob.c
+++ fun/superglob.c
@@ -1,5 +1,16 @@
 /**
  * Superglob! Build a "minimal" glob matching a list of entries
+ *
+ * Copyright (c) 2010, Red Hat Inc.
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided that
+ * the above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * Maintainer: Pierre Carrier <prc@redhat.com>
+ *
+ * TODO:
+ * - Find an actual use case?
 **/
 
 #include <stdlib.h>
