7537346363294e467e5586ea11f8544a281b8aa7
Pierre Carrier <pierre@spotify.com>
2011-08-28 17:20:45 +0200

    [adm] l-w-m-v.rb: use device and inode in lib descriptions

--- adm/libs-with-multiple-versions.rb
+++ adm/libs-with-multiple-versions.rb
@@ -20,25 +20,34 @@ pids = Dir.new("/proc").entries.grep /^[0-9]+$/
 pids.each do |p|
 	pid = p.to_i
 	name = IO::read("/proc/#{pid}/cmdline")[0..-1].gsub /\0/, ' '
-	libs = IO::read("/proc/#{pid}/maps").scan(/\/[^ ]+\.so[^ \/]*(?: \(deleted\))?$/).uniq
+	liblines = 
+	libs = Hash[
+		IO::read("/proc/#{pid}/maps").lines.grep(/\.so/).collect do |line|
+			fields = line.split
+			devinode = "#{fields[3]} #{fields[4]}"
+			path = fields[5..-1].join ' '
+			next [devinode, path]
+		end
+	]
 	infos[pid] = ProcInfo.new name, libs
 end
 
 libs = {}
 infos.each do |pid, info|
-	info[:libs].each do |path|
+	info[:libs].each do |devinode, path|
 		name = libname path
+		descr = "[#{devinode}]#{path}"
 		libs[name] ||= {}
-		libs[name][path] ||= []
-		libs[name][path] << pid
+		libs[name][descr] ||= []
+		libs[name][descr] << pid
 	end
 end
 
-puts "Libraries: #{libs.keys.join ', '}"
+puts "Libraries: #{libs.keys.join ','}"
 
 libs.reject { |name, versions| versions.length == 1 }.each do |name, versions|
 	$stderr.puts "#{name} has multiple versions:"
-	versions.each do |path, pids|
-		$stderr.puts "  #{path} used by PIDs #{pids.join ', '}"
+	versions.each do |descr, pids|
+		$stderr.puts "  #{descr} used by PIDs #{pids.join ','}"
 	end
 end
