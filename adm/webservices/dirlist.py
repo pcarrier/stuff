dfc310cef5648a949cd58c2308c2cd277b4f857a
Pierre Carrier <pierre@spotify.com>
2011-08-29 00:58:48 +0200

    [adm] add webservices/dirlist.py

---
+++ adm/webservices/dirlist.py
@@ -0,0 +1,18 @@
+# List every filename that doesn't contain a dot in files/
+# Put bottle.py in the same directory
+from sys import path
+from os import listdir
+from os.path import dirname, join
+
+path.append(dirname(__file__))
+from bottle import route, run, default_app
+
+@route('/envs')
+def envs():
+    return "\n".join(
+        filter(lambda name: '.' not in name,
+            listdir(join(dirname(__file__), 'dataroot'))
+        )
+    )+"\n"
+
+application = default_app()
