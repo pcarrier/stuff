928a900d2b78f20610fa61ad37f0d590e283ad7c
Pierre Carrier <pierre@spotify.com>
2012-03-09 16:21:45 +0000

    [adm] dpkg/conf-status.rb: sorted output

--- adm/dpkg/conf-status.rb
+++ adm/dpkg/conf-status.rb
@@ -7,10 +7,11 @@
 
 require 'digest/md5'
 
-files_and_sums = %x[dpkg-query -Wf '${Conffiles}'].split.find_all {|w| w != 'obsolete'}
+out = %x[dpkg-query -Wf '${Conffiles}']
+files_and_sums = out.split.find_all {|w| w != 'obsolete'}
 file_sum_hash = Hash[*files_and_sums]
 
-file_sum_hash.each { |file, pkgsum|
+file_sum_hash.sort.each { |file, pkgsum|
   if File.exists? file
     fssum = Digest::MD5.hexdigest IO::read file
     if fssum == pkgsum
