6bd8a338bb4a8f6613dc8ee65843fcd012961a33
Pierre Carrier <pierre@spotify.com>
2012-10-22 23:10:21 +0200

    moving small ruby utilities to the baze gem

--- adm/randmac.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-printf "%02x:%02x:%02x:%02x:%02x:%02x\n", *(6.times.map{rand(256)})
--- fun/hex2ip.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-n = ARGV[0].hex
-d = n % 256
-c = (n >> 8) % 256
-b = (n >> 16) % 256
-a = (n >> 24) % 256
-puts "#{a}.#{b}.#{c}.#{d}"
