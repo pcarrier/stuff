2e9b6529899e8b507158a5ed509130ffda7a2da1
Pierre Carrier <pierre@spotify.com>
2012-11-23 03:19:12 +0100

    fun/firemeplz.c: more advanced tests

--- fun/firemeplz.c
+++ fun/firemeplz.c
@@ -1,42 +1,69 @@
 /* Perfect for your production database servers. */
 
 #include <errno.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 #define EVER (;;)
 
 enum {
-	EXIT_OK = 0,
-	EXIT_USAGE,
-	EXIT_FAILED,
+    EXIT_OK = 0,
+    EXIT_USAGE,
+    EXIT_FAILED
 } exit_code;
 
-int main(int argc, char **argv) {
-	long incr;
-	void *h;
-	const char DOT = '.';
+int main()
+{
+    char *pages_per_iteration_c, *h;
+    intptr_t page_size, alloc_per_iteration, start, current, allocated;
+    long pages_per_iteration = 1;
+    bool pollute = false;
 
-	if (argc !=2)
-		exit(EXIT_USAGE);
+    start = (intptr_t) sbrk(0);
 
-	errno = 0;
-	incr = strtol(argv[1], NULL, 10);
-	if (errno != 0) {
-		perror("strtol");
-		exit(EXIT_USAGE);
-	}
+    page_size = (intptr_t) sysconf(_SC_PAGESIZE);
+    fprintf(stderr, "page size: %li\n", (unsigned long) page_size);
 
-	for EVER {
-		h = malloc(incr);
+    /* Settings */
+    if (getenv("POLLUTE") != NULL)
+        pollute = true;
 
-		if (!h) {
-			perror("malloc");
-			exit(EXIT_FAILED);
-		}
+    pages_per_iteration_c = getenv("PAGES");
+    if (pages_per_iteration_c != NULL) {
+        errno = 0;
+        pages_per_iteration = strtol(pages_per_iteration_c, NULL, 10);
+        if (errno != 0) {
+            perror("strtol");
+            exit(EXIT_USAGE);
+        }
+    }
 
-		write(STDOUT_FILENO, &DOT, 1);
-	}
+    alloc_per_iteration = pages_per_iteration * page_size;
+    allocated = 0;
 
-	return (EXIT_OK);
+    for EVER {
+        h = (char *) malloc(alloc_per_iteration);
+        current = (intptr_t) sbrk(0);
+        allocated += pages_per_iteration;
+
+        if (h == NULL) {
+            perror("malloc");
+            exit(EXIT_FAILED);
+        }
+
+        if (pollute == true) {
+            intptr_t i;
+            for (i = 0; i < pages_per_iteration; i++) {
+                *h = 1;
+                h += page_size;
+            }
+        }
+
+        fprintf(stdout, "allocated %li, segment %li..\n",
+                allocated, (current - start) / page_size);
+    }
+
+    return (EXIT_OK);
 }
